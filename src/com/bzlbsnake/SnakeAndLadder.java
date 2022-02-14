677698c1b619a50cfba766917e0c7264e89e61f7
first_name <ss329222@gmail.com>
2022-02-14 16:59:06 +0530

    Added:RepeatTill100

--- src/com/bzlbsnake/SnakeAndLadder.java
+++ src/com/bzlbsnake/SnakeAndLadder.java
@@ -9,23 +9,29 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int position = 0;
-		Random rand = new Random();
-		int dice = rand.nextInt(6) + 1;
-		System.out.println("dice:" + dice);
-		int optionCheck = rand.nextInt(3);
-		System.out.println("optionCheck: " + optionCheck);
-
-		if (optionCheck == LADDER) {
-			System.out.println("ladder!!!");
-			position = position + dice;
-		} else if (optionCheck == SNAKE && position > dice) {
-			System.out.println("snake!!!");
-			position = position - dice;
-		} else {
-			System.out.println("noPlay!!!");
-			position = position;
-		}
 		System.out.println("position: " + position);
-	}
+		while (position < 100) {
+			Random rand = new Random();
+			int dice = rand.nextInt(6) + 1;
+			System.out.println("dice:" + dice);
+			int optionCheck = rand.nextInt(3);
+			System.out.println("optionCheck: " + optionCheck);
 
+			if (optionCheck == LADDER) {
+				System.out.println("ladder!!!");
+				position = position + dice;
+			} else if (optionCheck == SNAKE) {
+				System.out.println("snake!!!");
+				position = position - dice;
+			} else {
+				System.out.println("noPlay!!!");
+				position = position;
+			}
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("position: " + position);
+		}
+
+	}
 }
