6204abca3dde98aef5fb6413bf3002bd5694e94d
first_name <ss329222@gmail.com>
2022-02-14 16:41:15 +0530

    Added:DiceNumberBetween1to6

--- src/com/bzlbsnake/SnakeAndLadder.java
+++ src/com/bzlbsnake/SnakeAndLadder.java
@@ -1,9 +1,15 @@
 package com.bzlbsnake;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		int position1 = 0;
+		int position = 0;
+		Random rand = new Random();
+		int dice = rand.nextInt(6) + 1;
+		System.out.println("dice:" + dice);
+
 	}
 
 }
